23ebf064b16152b369a415ea1953b85d11ac7262
Abhishek Shrivastav <abhishekshrivastav1920@gmail.com>
2020-12-24 23:35:21 +0530

    hashing

---
+++ Arrays/twin_count.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+        int countWords(string list[], int n)
+        {
+           //code here.
+           int counter{0};
+           
+           unordered_map <string,int> map;
+           
+           for(int i=0;i<n;i++)
+                {
+                    map[list[i]] = map[list[i]] + 1; 
+                }
+                for(auto &i : map)
+                {
+                    if(i.second == 2)
+                        counter++;
+                }
+                return counter;
+        }
+
+};
+
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string list[n];
+        for(int i=0;i<n;i++)
+            cin>>list[i];
+        Solution ob;    
+        cout <<ob.countWords(list, n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
